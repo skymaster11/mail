be5ae0c12b2ed13feed317cb83cfd548f85e5fea
skymaster11 <69782767+skymaster11@users.noreply.github.com>
2020-08-22 22:48:54 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+require 'PHPMailer.php';
+require 'SMTP.php';
+require 'Exception.php';
+require 'OAuth.php';
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+$mail->isSMTP();
+/*
+Enable SMTP debugging
+0 = off (for production use)
+1 = client messages
+2 = client and server messages
+*/
+$mail->SMTPDebug = 0;
+$mail->Host = 'smtp.live.com';
+$mail->Port = 25;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "rwyseguridad@hotmail.com";
+$mail->Password = "Correo12345";
+$mail->setFrom('rwyseguridad@hotmail.com', 'Picha Mama');
+$mail->addAddress('d.josue@hotmail.es', 'Panocha');
+$mail->Subject = 'Rica la carne';
+$mail->Body = "Sirvio la picha";
+$mail->CharSet = 'UTF-8'; // Con esto ya funcionan los acentos
+$mail->IsHTML(true);
+
+if (!$mail->send())
+{
+	echo "Error al enviar el E-Mail: ".$mail->ErrorInfo;
+}
+else
+{
+	echo "E-Mail enviado";
+}
+?>
