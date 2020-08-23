0ed4cab468c7a4862d6414de1e0e5c23860c2175
skymaster11 <69782767+skymaster11@users.noreply.github.com>
2020-08-22 23:08:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ Enable SMTP debugging
 */
 $mail->SMTPDebug = 0;
 $mail->Host = 'smtp.live.com';
-$mail->Port = 465;
+$mail->Port = 25;
 $mail->SMTPSecure = 'tls';
 $mail->SMTPAuth = true;
 $mail->Username = "rwyseguridad@hotmail.com";
